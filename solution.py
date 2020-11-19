d29d32ab8b636c80bef53448440c8892679fa22b
Kyle Mitra <kjmitra@yahoo.com>
2020-11-19 15:54:12 -0500

    Wrote create_pixel_list function to generate a list of pixels needed

--- solution.py
+++ solution.py
@@ -5,10 +5,6 @@ GET_URL = "https://www.random.org/integers/"  # Address to integer generator
 
 
 def generate_integers(amount: int):
-    # Check that amount if valid
-    if amount < 1 or amount > 10000:
-        print('Amount must be in range 1-10000')
-
     # Create parameters for API request input
     my_params = {
         'num': str(amount),  # The number of integers requested
@@ -30,9 +26,28 @@ def generate_integers(amount: int):
         return [int(num) for num in response.text.split()]
 
 
-def generate_image(pixel_list):
-    new_image = Image.new('RGB', (120, 120))
+def generate_image():
+    new_image = Image.new('RGB', (120, 120))  # Creates new image
     return
+
+
+def create_pixel_list():
+    pixel_list = []  # Create new list to hold pixels
+    num_pixels = 120 * 120  # 120 x 120 image size
+    total_values = num_pixels * 3  # 3 RGB values per pixel
+
+    # NOTE: Since the maximum input of the Integer Generator is
+    # 10,000, we need to consider the possibility for when there
+    # are more than 10,000 values we need to generate
+    while total_values > 10000:
+        # Add 10,000 random generated integers to end of pixel list
+        pixel_list.extend(generate_integers(10000))
+        # Decrement total values by 10,000 to get remaining values needed
+        total_values -= 10000
+
+    # Once total_values < 10,000 add remaining values needed to pixel list
+    pixel_list.extend(generate_integers(total_values))
+    return pixel_list
 
 
 def main():
