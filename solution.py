16aa5deab10937cef07c912d71b803b45a8f0a2b
Kyle Mitra <kjmitra@yahoo.com>
2020-11-19 16:17:22 -0500

    Wrote generate_image function

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import requests
 from PIL import Image
+import numpy as np
 
 GET_URL = "https://www.random.org/integers/"  # Address to integer generator
 
@@ -10,35 +11,32 @@ def generate_integers(amount: int):
         'num': str(amount),  # The number of integers requested
         'min': '0',  # For RGB image, we want between 0-255
         'max': '255',  # For RGB image, we want between 0-255
-        'col': '3',  # Want Standard 3 column output
+        'col': '3',  # Columns for Red, Green, Blue Values
         'base': '10',  # Want Base 10 Numbers
         'format': 'plain',  # Want plain text output
         'rnd': 'new'  # Standard randomized format
         }
+    print(my_params)
 
     # Make GET Request using parameters above
     response = requests.get(GET_URL, params=my_params)
 
     # Check status code for potential errors
     if response.status_code != 200:
-        print(response.status_code + "Error")
+        print(str(response.status_code) + " Error")
     else:  # Return numbers if no error found
         return [int(num) for num in response.text.split()]
-
-
-def generate_image():
-    new_image = Image.new('RGB', (120, 120))  # Creates new image
-    return
 
 
 def create_pixel_list():
     pixel_list = []  # Create new list to hold pixels
     num_pixels = 120 * 120  # 120 x 120 image size
     total_values = num_pixels * 3  # 3 RGB values per pixel
+    # total_values = 43,200
 
     # NOTE: Since the maximum input of the Integer Generator is
-    # 10,000, we need to consider the possibility for when there
-    # are more than 10,000 values we need to generate
+    # 10,000, we need to account for the fact that we require more than
+    # the allowed amount of values.
     while total_values > 10000:
         # Add 10,000 random generated integers to end of pixel list
         pixel_list.extend(generate_integers(10000))
@@ -50,9 +48,14 @@ def create_pixel_list():
     return pixel_list
 
 
-def main():
-    return
+def generate_image():
+    pixel_list = create_pixel_list()  # Generate random list of pixels
+    np_pixel_array = np.array(pixel_list)  # Create numpy array of pixels
+    np_pixel_array = np_pixel_array.reshape((120*120, 3))  # Resize numpy array
+    new_image = Image.new('RGB', (120, 120))  # Creates new image
+    new_image.putdata(tuple(pixel) for pixel in np_pixel_array)  # Input pixels
+    new_image.save("my_RBG_image.png")  # Save image
 
 
 if __name__ == '__main__':
-    main()
+    generate_image()
